da40f34715ac30d271fe54ed6be60a2aafb928cf
lentg <lentg@qq.com>
2023-01-10 13:15:05 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ const port = parseInt(process.env.PORT || "8080");
 
 import { Database } from "bun:sqlite";
 
-const db = new Database("/home/mydb.sqlite");
+const db = new Database("mydb.sqlite");
 
-// db.run(
-//   "CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY AUTOINCREMENT, greeting TEXT)",
-// );
-// db.run("INSERT INTO foo (greeting) VALUES (?)", "Welcome to bun!");
-// db.run("INSERT INTO foo (greeting) VALUES (?)", "Hello World!");
+db.run(
+  "CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY AUTOINCREMENT, greeting TEXT)",
+);
+db.run("INSERT INTO foo (greeting) VALUES (?)", "Welcome to bun!");
+db.run("INSERT INTO foo (greeting) VALUES (?)", "Hello World!");
 
 
 app.get("/", async (ctx) => {
@@ -20,10 +20,10 @@ app.get("/", async (ctx) => {
   return ctx.sendJson({ rs: rs, ms: Date.now() - t1 })
 });
 
-// app.get("/add", async (ctx) => {
-//   db.run("INSERT INTO foo VALUES (?)", "Hello World! " + Date.now());
-//   return ctx.sendText("OK: " + Date.now());
-// })
+app.get("/add", async (ctx) => {
+  db.run("INSERT INTO foo VALUES (?)", "Hello World! " + Date.now());
+  return ctx.sendText("OK: " + Date.now());
+})
 
 const server = app.listen({ port: port });
 console.log(`Server listening on ${server.hostname}:${port}`);
