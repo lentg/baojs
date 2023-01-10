0eca3abff173179d1271d8464fef7c9fa4164990
lentg <lentg@qq.com>
2023-01-10 12:14:32 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,19 @@ import { Database } from "bun:sqlite";
 
 const db = new Database("mydb.sqlite");
 
-// db.run(
-//   "CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY AUTOINCREMENT, greeting TEXT)",
-// );
-// db.run("INSERT INTO foo (greeting) VALUES (?)", "Welcome to bun!");
-// db.run("INSERT INTO foo (greeting) VALUES (?)", "Hello World!");
+db.run(
+  "CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY AUTOINCREMENT, greeting TEXT)",
+);
+db.run("INSERT INTO foo (greeting) VALUES (?)", "Welcome to bun!");
+db.run("INSERT INTO foo (greeting) VALUES (?)", "Hello World!");
 
 
 app.get("/", async (ctx) => {
   let t1 = Date.now()
   let rs = await db.query("SELECT * FROM foo").all();
 //   return JSON.stringify(rs)
-  return ctx.sendPrettyJson({data: rs, ms: Date.now() - t1});
+//   return ctx.sendJson({data: rs, ms: Date.now() - t1});
+  return ctx.sendJson({ rs: rs, ms: Date.now() - t1 })
 //   return ctx.sendText("Hello world from Bao.js running on Railway!" + Date.now());
 });
 
